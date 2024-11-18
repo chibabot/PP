83c6228e10c09b8f6e7d5a47c5007342b9f7480c
chibabot <75862756+chibabot@users.noreply.github.com>
2024-11-18 17:16:31 +0300

    Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Func<Action<bool>, float, float> lambda = Function;
+        Action<bool> action = Method;
+
+        Console.WriteLine(lambda(action, 4.0f)); // True
+        Console.ReadLine();
+        Console.WriteLine(lambda(action, 4.5f)); // False
+        Console.ReadLine();
+        Console.WriteLine(lambda(action, 3.0f)); // False
+        Console.ReadLine();
+        Console.WriteLine(lambda(action, 3.5f)); // False
+        Console.ReadLine();
+    }
+    static void Method(bool s)
+    {
+        Console.WriteLine(s); // Вывод логического выражения
+    }
+
+    static float Function(Action<bool> action, float num)
+    {
+        //Проверка на четность числа с плавающей точкой
+        float res = num % 2;
+        action(res==0); // Проверка на четность
+        return res; // Вывод остатка от деления на 2
+    }
+}
