f87067e80ab3108212b8cd94c6027d629c14c6f0
chibabot <75862756+chibabot@users.noreply.github.com>
2024-11-18 17:22:04 +0300

    Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        Func<int[], int, int> lambda = countSubsets;
+        var arr = new int[15];
+        for (int i = 0; i < arr.Length; i++)
+            arr[i] = rand.Next(0, 15);
+        int n = arr.Length;
+        var str = string.Join(" ", arr);
+        Console.WriteLine(str);
+        Console.WriteLine("Number of subsets = " + countSubsets(arr, n));
+    }
+    static int countSubsets(int[] arr, int n)
+    {
+        HashSet<int> us = new HashSet<int>();
+        int even_count = 0;
+        for (int i = 0; i < n; i++)
+            if (arr[i] % 2 == 0)
+                us.Add(arr[i]);
+        even_count = us.Count;
+        return (int)(Math.Pow(2, even_count) - 1);
+    }
+}
